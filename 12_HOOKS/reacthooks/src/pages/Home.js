d1fd2d471f4378ecc0dd514b3ac935ff344b9b57
Luanaap <57630311+Luanaap@users.noreply.github.com>
2024-06-04 22:27:16 -0300

    Finalizando sessão de hooks

--- 12_HOOKS/reacthooks/src/pages/Home.js
+++ 12_HOOKS/reacthooks/src/pages/Home.js
@@ -4,6 +4,11 @@ import HookUseReducer from '../components/HookUseReducer'
 import HookUseEffect from '../components/HookUseEffect'
 import { SomeContext } from '../components/HookUseContext'
 import HookUseRef from '../components/HookUseRef'
+import HookUseCallback from '../components/HookUseCallback'
+import HookUseMemo from '../components/HookUseMemo'
+import HookUseLayoutEffect from '../components/HookUseLayoutEffect'
+import HookUseImperativeHandle from '../components/HookUseImperativeHandle'
+import HookCustom from '../components/HookCustom'
 
 const Home = () => {
   const {contextValue} = useContext(SomeContext);
@@ -17,6 +22,11 @@ const Home = () => {
       <p>Valor do context: {contextValue}</p>
       <hr/>
       <HookUseRef></HookUseRef>
+      <HookUseCallback></HookUseCallback>
+      <HookUseMemo></HookUseMemo>
+      <HookUseLayoutEffect></HookUseLayoutEffect>
+      <HookUseImperativeHandle></HookUseImperativeHandle>
+      <HookCustom/>
     </div>
   )
 }
