5e5623d54a0d848e5cd647d749efac1f15d9f1a5
Luana <l.andrade2711@gmail.com>
2024-10-04 20:05:29 -0300

    adicionando redux
    adicionando redux

--- 13_REACTGRAM/frontend/src/pages/Auth/Register.js
+++ 13_REACTGRAM/frontend/src/pages/Auth/Register.js
@@ -2,15 +2,24 @@ import "./Auth.css";
 
 //componentes
 import { Link } from "react-router-dom";
+import Message from "../../components/Message";
 
 //Hooks
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+//Redux
+import { register, reset} from "../../slices/authSlice";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const {loading, error} = useSelector((state) => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,7 +32,14 @@ const Register = () => {
     };
   
     console.log(user);
+
+    dispatch(register(user));
   }
+
+  //Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
 
   return <div id="register">
     <h2>ReactGram</h2>
@@ -49,10 +65,22 @@ const Register = () => {
         placeholder="Confirme a senha" 
         onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword || ""}
       />
-      <input 
+     {
+      !loading && 
+        <input 
         type="submit" 
         placeholder="Cadastrar"
       />
+     }
+     {
+      loading && 
+        <input 
+        type="submit" 
+        placeholder="Aguarde..."
+        disabled
+      />
+     }
+      {error && <Message msg={error} type="error" />}
     </form>
     <p>Já tem conta? <Link to="/login">Clique aqui.</Link></p>
   </div>
